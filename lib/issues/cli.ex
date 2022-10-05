db08de2e8b6736e0c748522b6b155aefe3973b32
l1nefeed <l1nefeed@gmail.com>
2022-10-05 17:44:43 +0900

    P247 까지

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,10 +1,11 @@
 defmodule Issues.Cli do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
   @default_count 4
   @moduledoc """
   명령줄 파싱을 수행한 뒤, 각종 함수를 호출해
   깃허브 프로젝트의 최근 _n_개 이슈를 표 형식으로 만들어 출력한다.
   """
-  def run(argv) do
+  def main(argv) do
     # parse_argv(argv)
     argv
     |> parse_argv
@@ -54,9 +55,18 @@ defmodule Issues.Cli do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse()
   end
 
   def decode_response({:ok, body}), do: body
@@ -65,4 +75,10 @@ defmodule Issues.Cli do
     System.halt(2)
   end
 
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
+  end
 end
